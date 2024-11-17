f0baa2357199a29aff9fff03db221827ed7a1c27
Marcos Felipe <30422283+djmarcosban@users.noreply.github.com>
2024-11-17 03:20:54 +0000

    atualizando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>A analogia do treinador de futebol: pessoas certas nas posições certas!</h1>
+    return <h1>A analogia do treinador de futebol: pessoas certas nas posições certas.</h1>
 }
 
 export default Home;
