6803f3dfabe23698b71834da7845aa65f65d8e2b
Marcos Felipe <30422283+djmarcosban@users.noreply.github.com>
2024-11-20 02:08:43 +0000

    adiciona arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1>A analogia do treinador de futebol: pessoas certas nas posições certas.</h1>
+  return <h1>A analogia do treinador de futebol: pessoas certas nas posições certas.</h1>
+
 }
 
 export default Home;
