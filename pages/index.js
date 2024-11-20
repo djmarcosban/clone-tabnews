df7bad2d8ef1800a7cb9ab130838bdeef92c6142
Marcos Felipe <30422283+djmarcosban@users.noreply.github.com>
2024-11-20 02:30:54 +0000

    prettier

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 function Home() {
-  return <h1>A analogia do treinador de futebol: pessoas certas nas posições certas.</h1>
+  return (
+    <h1>
+      A analogia do treinador de futebol: pessoas certas nas posições certas.
+    </h1>
+  );
+}
 
+function Teste() {
+  console.log('teste')
 }
 
 export default Home;
